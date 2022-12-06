4356403703a84d3706e2c0e6b6428e4997b48506
Adam Rackis <adamrackis@hotmail.com>
2022-12-06 16:01:19 -0600

    tweak repro

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -2,9 +2,6 @@ import { invalid } from '@sveltejs/kit';
 
 export const actions = {
 	async save1({ request }) {
-		throw new Error('Bad');
-	},
-	async save2({ request }) {
-		return invalid(500);
+		console.log('SAVED, BABY');
 	}
 };
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,31 +1,17 @@
 <script>
 	import { enhance } from '$app/forms';
-
-	function fn() {
-		return async () => {};
-	}
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 
-<h2>Throw new Error()</h2>
-<form method="post" action="?/save1">
+<h2>RawDog form (won't save)</h2>
+<form action="?/save1">
 	<input name="a" />
 </form>
 
-<h2>Throw new Error() with use:enhance</h2>
-<form method="post" action="?/save1" use:enhance>
+<h2>PE form (will save)</h2>
+<form action="?/save1" use:enhance>
 	<input name="a" />
-</form>
-
-<h2>Throw new Error() with use:enhance with callback</h2>
-<form method="post" action="?/save1" use:enhance={fn}>
-	<input name="a" />
-</form>
-
-<h2>return invalid()</h2>
-<form method="post" action="?/save2">
-	<input name="b" />
 </form>
 
 <style>
